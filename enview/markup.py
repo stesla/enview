3ed03f99ba54c8056b9475e74ebf79d650977436
Samuel Tesla <samuel.tesla@gmail.com>
2021-01-09 19:29:11 -0600

    add 256-color support

--- enview/markup.py
+++ enview/markup.py
@@ -64,30 +64,74 @@ class parseCSI:
 
     def __call__(self, c, t, ts):
         if c == 'm':
-            for code in self.code.split(';'):
-                c = int(code)
-                if c == 0:
+            codes = self.code.split(';')
+            while codes:
+                c = int(codes.pop(0))
+                if 0 == c:
                     t.fg = None
                     t.bg = None
                     t.bold = False
-                if c == 1:
+                elif 1 == c:
                     t.bold = True
                 elif 30 <= c and c <= 37:
-                    t.fg = simplecolor[c - 30]
+                    t.fg = LOW[c - 30]
+                elif 38 == c and 5 == int(codes.pop(0)):
+                    t.fg = color8bit(codes.pop(0)) 
                 elif 40 <= c and c <= 47:
-                    t.bg = simplecolor[c - 40]
+                    t.bg = LOW[c - 40]
+                elif 48 == c and 5 == int(codes.pop(0)):
+                    t.bg = color8bit(codes.pop(0)) 
+                elif 90 <= c and c <= 97:
+                    t.fg = HIGH[c - 90]
+                elif 100 <= c and c <= 107:
+                    t.bg = HIGH[c - 100]
             return t, parsePlain
         else:
             self.code += c
             return t, self
 
-simplecolor = [
-    '#505050', # display black as a dark gray
-    'red',
-    'green',
-    'yellow',
-    'blue',
-    'magenta',
-    'cyan',
-    'white',
+LOW = [
+    '#303030', # black
+    '#800000', # red
+    '#008000', # green
+    '#808000', # yellow
+    '#000080', # blue
+    '#800080', # magenta
+    '#008080', # cyan
+    '#c0c0c0', # white
 ]
+
+HIGH = [
+    '#808080', # black
+    '#ff0000', # red
+    '#00ff00', # green
+    '#ffff00', # yellow
+    '#0000ff', # blue
+    '#ff00ff', # magenta
+    '#00ffff', # cyan
+    '#ffffff', # white
+]
+
+# i = n - 232
+GRAYSCALE = [
+        '#080808','#121212','#1c1c1c','#262626','#303030','#3a3a3a',
+        '#444444','#4e4e4e','#585858','#626262','#6c6c6c','#767676',
+        '#808080','#8a8a8a','#9e9e9e','#9e9e9e','#a8a8a8','#b2b2b2',
+        '#bcbcbc','#c6c6c6','#d0d0d0','#dadada','#e4e4e4','#eeeeee',
+        ]
+
+CUBE = ['00','5f','87','af','d7','ff']
+
+def color8bit(code):
+    n = int(code)
+    if 0 <= n and n <= 7:
+        return LOW[n]
+    elif 8 <= n and n <= 15:
+        return HIGH[n - 8]
+    elif 16 <= n and n <= 231:
+        r = (n - 16) // 36
+        g = (n - 16 - 36*r) // 6
+        b = n - 16 - 36*r - 6*g
+        return '#' + CUBE[r] + CUBE[g] + CUBE[b]
+    elif 232 <= n and n <= 255:
+        return GRAYSCALE[n - 232]
--- tests/test_markup.py
+++ tests/test_markup.py
@@ -6,24 +6,40 @@ def test_parse_empty():
 def test_parse_plain():
     assert parse('plaintext') == [Text('plaintext')]
 
-def test_parse_simplecolors():
-    assert parse('some\x1b[36;44mcolor') == [Text('some'), Text('color', bg='blue', fg='cyan')]
+def test_parse_low_colors():
+    assert parse('some\x1b[36;44mcolor') == [Text('some'), Text('color', bg='#000080', fg='#008080')]
+
+def test_parse_low_colors():
+    assert parse('some\x1b[96;104mcolor') == [Text('some'), Text('color', bg='#0000ff', fg='#00ffff')]
+
+def test_parse_256_low():
+    assert parse('\x1b[38;5;1;48;5;2mword') == [Text('word', bg='#008000', fg='#800000')]
+
+def test_parse_256_high():
+    assert parse('\x1b[38;5;8;48;5;9mword') == [Text('word', bg='#ff0000', fg='#808080')]
+
+def test_parse_color_cube():
+    assert parse('\x1b[38;5;16mword') == [Text('word', fg='#000000')]
+    assert parse('\x1b[38;5;42mword') == [Text('word', fg='#00d787')]
+
+def test_parse_grayscale():
+    assert parse('\x1b[38;5;243mword') == [Text('word', fg='#767676')]
 
 def test_parse_reset():
-    assert parse('foo\x1b[31mbar\x1b[0mbaz') == [Text('foo'), Text('bar', fg='red'), Text('baz')]
+    assert parse('foo\x1b[31mbar\x1b[0mbaz') == [Text('foo'), Text('bar', fg='#800000'), Text('baz')]
 
 def test_parse_bold():
     assert parse('\x1b[1mfoo') == [Text('foo', bold=True)]
 
 def test_parse_multiple_sequences():
-    assert parse('\x1b[1m\x1b[33mfoo') == [Text('foo', bold=True, fg='yellow')]
+    assert parse('\x1b[1m\x1b[33mfoo') == [Text('foo', bold=True, fg='#808000')]
 
 def test_html_plain():
     assert Text('foo').to_html() == 'foo'
 
 def test_html_all_attributes():
-    assert Text('foo', bg='yellow', fg='red', bold=True).to_html() == \
-            '<span style="background-color: yellow; color: red; font-weight: bold">foo</span>'
+    assert Text('foo', bg='#808000', fg='#800000', bold=True).to_html() == \
+            '<span style="background-color: #808000; color: #800000; font-weight: bold">foo</span>'
 
 def test_html_escaped():
     assert Text('<title>').to_html() == '&lt;title&gt;'
