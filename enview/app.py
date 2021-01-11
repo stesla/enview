8e090126352065cce1bb0e63ae37f598b17fb3b2
Samuel Tesla <samuel.tesla@gmail.com>
2021-01-11 10:06:52 -0600

    add breadcrumbs, use divide-y

--- enview/app.py
+++ enview/app.py
@@ -19,13 +19,20 @@ def logs(path):
                     key=lambda f: f.path, reverse=True)
         files = sorted([file for file in ps if not file.isdir],
                     key=lambda f: f.mtime, reverse=True)
-        return render_template('directory.html', dirs=dirs, files=files)
+        return render_template('directory.html', crumbs=crumbs(path), dirs=dirs, files=files)
     else:
         with Log(path).open() as f:
             text = f.read()
         html = ''.join(t.to_html() for t in parse(text))
-        return render_template('log.html', html=html)
+        return render_template('log.html', crumbs=crumbs(path), path=path, html=html)
 
 @app.template_filter()
 def datetime(value):
     return time.strftime("%Y-%m-%d %H:%M:%S", value)
+
+def crumbs(path):
+    list = []
+    while path:
+        list.append((path, os.path.basename(path)))
+        path = os.path.dirname(path)
+    return reversed(list)
